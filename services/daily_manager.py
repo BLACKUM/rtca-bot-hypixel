fa489f9a924b43c8fb83692f506d6c64c7c2c60f
Artem <Zipmaster2852@gmail.com>
2026-01-02 21:19:45 +0300

    Added a couple of gifs

--- services/daily_manager.py
+++ services/daily_manager.py
@@ -51,6 +51,23 @@ CONGRATS_GIFS = [
     "https://i.imgflip.com/6f5788.gif",
     "https://media2.giphy.com/media/v1.Y2lkPTZjMDliOTUyODIyMXFzamljbm9sc2d1NnVzdDJvYXllN2Jjcm14Y25kYm00cGF5ZSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/bum2UBz4nR9IxZmWde/giphy-downsized.gif",
     "https://media3.giphy.com/media/v1.Y2lkPTZjMDliOTUyODVzb2JyeXpveng0Z2FocHdsczF6enZqbDN6NDBiZmhuaWlndXl1NiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/JyOtBwVBKFoeIQ14Po/200w_d.gif"
+    "https://media.discordapp.net/attachments/1122856048374595647/1152961363614908497/JIXMersMo8xcnmXQ.gif?ex=69591b72&is=6957c9f2&hm=40759ab651245f54ba2e5f10640403b43806c1cfe5cbf542ec4be9d2a4607437&=&width=585&height=75",
+    "https://media.discordapp.net/attachments/1263938847910269000/1276128862849339393/minus-infinite-social-credit-china.gif?ex=6958f21e&is=6957a09e&hm=34acdfe4c04a23c4d4e4256f908b7523d41670e382018038544bfc3aeb2823ff&=&width=800&height=450",
+    "https://media.discordapp.net/attachments/1263938847910269000/1276125652730380349/gypsycrusadervshoke.gif?ex=6958ef20&is=69579da0&hm=545b63f83cb383740758971ca6940b9f9b22d1bd9d832ed09089d9246c15ffc0&=&width=800&height=503",
+    "https://media.discordapp.net/attachments/1263938847910269000/1276130366847057950/redditsave.com_y2dkx7x76f481.gif?ex=6958f384&is=6957a204&hm=da6abb1ff3db36d77abaf35e8103173e849eb85297686fb2edcb1f1dec3cb57f&=&width=800&height=800",
+    "https://images-ext-1.discordapp.net/external/qZk6BMSzlnRRg_0-Qvr-FikuoRPmU8qyPoil_afHobs/https/i.imgur.com/leEaEIv.mp4",
+    "https://media.discordapp.net/attachments/1236050415020277840/1267879801566269481/caption.gif?ex=69594214&is=6957f094&hm=2581bd92a3109cad09926351f8b69d6bc279a5003b64536ddb50684697b5eb5d&=&width=750&height=934",
+    "https://media.discordapp.net/attachments/993269478060195950/1118188097222488104/ED6EFBC5-A68E-48C6-9103-B123B1ECC22D.gif?ex=69592a51&is=6957d8d1&hm=9b2954746a2d198cd8442724df3d8ccb09134436db77a0cdfdbc5b13cf79876e&=&width=254&height=60",
+    "https://cdn.discordapp.com/attachments/982414624844554241/1185533720233508894/tM1qwcbNS15DWrFk.gif?ex=6958f3d1&is=6957a251&hm=9708f3e09cc4e9d50d986acaf7086d2177ba9e1c4bc09a6b96214851f9a9c28a",
+    "https://media.discordapp.net/attachments/1255211359227084821/1269003922413060176/caption.gif?ex=6958bbc0&is=69576a40&hm=aec2497118642f46043347f8f462cc2f4e9fdd19d60cd0882d414949b84efcd8&=&width=294&height=375",
+    "https://media.discordapp.net/attachments/773213826186739775/979470724098039878/puzzlehater.gif.gif?ex=6958c972&is=695777f2&hm=f9d81391304ff5749a3deeea31c77a76627bb1b7c14c872e148fe28afd26344a&=&width=168&height=168",
+    "https://images-ext-1.discordapp.net/external/ZrS5lxO0iglXMgCx7TYp_cRzAG7DFDwrpCVJwJmmks8/https/i.imgur.com/jYv8BkK.mp4",
+    "https://media.discordapp.net/attachments/1347938901825814592/1400962571082924132/MedalTVMinecraft20250731185114-1753971386_1.gif?ex=6958e9d1&is=69579851&hm=dcaf2ed25e77aca670c8f26e47e332c73c3d202af44bb5f5140369b7eda85757&=&width=865&height=485",
+    "https://media.discordapp.net/attachments/1275995649988628574/1300897696269209681/wdhu.gif?ex=6958bf24&is=69576da4&hm=dc133d83ca5895f9975fee6ce031e2ad7bd4aad3cd84006b75de18ae3093abc7&=&width=658&height=849",
+    "https://media.discordapp.net/attachments/1172235640117669921/1402064324805132479/a276ic.gif?ex=6958f768&is=6957a5e8&hm=01f61333a5246f22918ca0791a5ea7950a9470ce5d55f8e55491c423eab243ec&=&width=450&height=236",
+    "https://media.discordapp.net/attachments/1397725617239363625/1403384070540365914/meowmeow.gif?ex=69592744&is=6957d5c4&hm=39de6c27bf9cca6fd7e61e2b61e8d232ce4bf1979e960d483204ba27e93b4605&=&width=445&height=445",
+    "https://media.discordapp.net/attachments/1379474866725453988/1420884081176084551/togif.gif?ex=6958e0ab&is=69578f2b&hm=11c34bd40104687de43199b1e43539f6f8fa59083525b3736cf39d1f8d66fc70&=&width=1545&height=875",
+    "https://media.discordapp.net/attachments/1420147559011192924/1447572753149591592/togif.gif?ex=6959122e&is=6957c0ae&hm=b55333c72e8ef1c3e5b9cc6247a0b414412b0d1e6e49973edb9dd3919d74b401&=&width=1024&height=796"
 ]
 
 class DailyManager:
